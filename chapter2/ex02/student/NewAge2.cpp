478ff403b1836ac14393c7a27ea7c3c20115d221
curb4387 <140212567+curb4387@users.noreply.github.com>
2024-08-29 00:54:20 +0000

    Testing Thu Aug 29 2024 00:54:20 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex02/student/NewAge2.cpp
+++ chapter2/ex02/student/NewAge2.cpp
@@ -1,1 +1,23 @@
 // This program calculates your age in the year 2050.
+// Input:  None
+// Output: Your current age followed by your age in 2050
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+   // Write your code here
+   int myNewAge;
+   int myCurrentAge = 26;
+   int currentYear = 2024;
+   const int YEAR = 2050;
+   myNewAge = myCurrentAge + (YEAR - currentYear);
+
+   cout << "My Current Age is " << myCurrentAge << "." << endl;
+   cout << "I will be " << myNewAge << " in YEAR." << endl;
+   
+   
+   return 0;
+}
