d9df610c6cb4cf736934297dddd601bc169db97f
curb4387 <140212567+curb4387@users.noreply.github.com>
2024-09-03 17:18:08 +0000

    Testing Tue Sep 03 2024 17:18:08 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex03/student/Furniture.cpp
+++ chapter2/ex03/student/Furniture.cpp
@@ -8,11 +8,11 @@ int main()
    string itemName = "TV Stand";
    double retailPrice = 325.00;
    double wholesalePrice = 200.00;
-   double salePrice = retailPrice - (retailPrice * 0.25);
-   double profit = retailPrice - wholesalePrice;
-   double saleProfit = salePrice - wholesalePrice;
 		
    // Write your assignment statements here
+   double salePrice = retailPrice - (retailPrice * 0.25);
+   double profit = retailPrice - wholesalePrice;
+   double saleProfit = salePrice - wholesalePrice;   
 
    cout << "Item Name: " << itemName << endl;
    cout << "Retail Price: $" << retailPrice << endl;
