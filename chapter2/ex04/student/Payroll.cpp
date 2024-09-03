43f71ba7664ab71c9e29600083125b73dccea818
curb4387 <140212567+curb4387@users.noreply.github.com>
2024-09-03 17:39:53 +0000

    Testing Tue Sep 03 2024 17:39:53 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex04/student/Payroll.cpp
+++ chapter2/ex04/student/Payroll.cpp
@@ -12,19 +12,19 @@ int main()
    double takeHomePay;
 
    // Calculate state tax here
-
+   stateTax = salary * 0.065;
    cout << "State Tax: $" << stateTax << endl;
    // Calculate federal tax here
-
+   federalTax = salary * 0.280;
    cout << "Federal Tax: $" << federalTax << endl;
    // Calculate dependent deduction here
-
+   dependentDeduction = (salary * 0.025) * numDependents;
    cout << "Dependents: $" << dependentDeduction << endl;
    // Calculate total withholding here
-
+   totalWithholding = stateTax + federalTax;
    cout << "Total Withholding: $" << totalWithholding << endl;
    // Calculate take-home pay here
-
+   takeHomePay = (salary - totalWithholding) + dependentDeduction;
    cout << "Salary: $" << salary << endl;
    cout << "Take-Home Pay: $" << takeHomePay << endl;
    return 0;
