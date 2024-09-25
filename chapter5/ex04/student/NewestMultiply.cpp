65d9b32d6def373696eff67088d4b49012971b21
curb4387 <140212567+curb4387@users.noreply.github.com>
2024-09-25 21:42:11 +0000

    Testing Wed Sep 25 2024 21:42:11 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex04/student/NewestMultiply.cpp
+++ chapter5/ex04/student/NewestMultiply.cpp
@@ -1,4 +1,4 @@
-/ NewestMultiply.cpp - This program prints the numbers 0 through 10 along
+// Multiply.cpp - This program prints the numbers 0 through 10 along
 // with these values multiplied by 2 and by 10.
 // Input:  None
 // Output: Prints the numbers 0 through 10 along with their values multiplied by 2 and by 10. 
@@ -15,11 +15,18 @@ int main()
    int numberCounter;  // Numbers 0 through 10
    int byTen;  // Stores the number multiplied by 10
    int byTwo;  // Stores the number multiplied by 2
-   const int NUM_LOOPS = 10;  // Constant used to control loop
+   const int MAX_NUM = 10;    // Constant used to control loop
 
    cout << "0 through 10 multiplied by 2 and by 10." << endl;
 
-   // Write your do while loop here		
-   
+   numberCounter = 0;
+   do {
+      byTen = numberCounter * 10;
+      byTwo = numberCounter * 2;
+      cout << numberCounter << " " << byTen << " " << byTwo << endl;
+      numberCounter++;
+   }
+   while (numberCounter <= MAX_NUM);
+
    return 0;
-}
+} 
