bc2ef2b8ba9caaf652a03dc19df269eda0a4d08e
curb4387 <140212567+curb4387@users.noreply.github.com>
2024-09-26 00:47:50 +0000

    Testing Thu Sep 26 2024 00:47:50 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex05/student/LetterE.cpp
+++ chapter5/ex05/student/LetterE.cpp
@@ -20,11 +20,11 @@ int main()
       for (column = 1; column <= NUM_ACROSS; column++) {
 
    // Decide when to print an asterisk in every column.
-         if (column == 1) 
+         if (column == 1 || column == 3 || column == 5) 
             cout << "*";
 
    // Decide when to print asterisk in column 1.   
-         else if (column == 3 || column == 5) 
+         else if (column == 1) 
             cout << "*";
 
    // Decide when to print a space instead of an asterisk.   
