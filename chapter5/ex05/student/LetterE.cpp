fdf8db7fc8a96587e20ce103122f5a663d0d34e5
curb4387 <140212567+curb4387@users.noreply.github.com>
2024-09-29 03:06:52 +0000

    Testing Sun Sep 29 2024 03:06:52 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex05/student/LetterE.cpp
+++ chapter5/ex05/student/LetterE.cpp
@@ -16,19 +16,19 @@ int main()
    // Write a loop to control the number of rows.
    for(row = 1; row <= NUM_DOWN; row++) {
    // Write a loop to control the number of columns
-      for(column = 1; column <= NUM_ACROSS; column++)
+      for(column = 1; column <= NUM_ACROSS; column++) {
    // Decide when to print an asterisk in every column.
-         if(row == 1 || row == 3 || row == 5)
+         if(row == 1 || row == 3 || row == 5) {
             cout << "*";
 
    // Decide when to print asterisk in column 1.   
-         else if(column == 1)
+         } else if(column == 1) {
             cout << "*";
 
    // Decide when to print a space instead of an asterisk.   
-         else
+         } else
             cout << " "; 
-
+      }
    // Figure out where to place this statement that prints a newline.
       cout << endl;
    }
