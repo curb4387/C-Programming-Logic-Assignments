0c8b8fea54137f610b78fc451b1b1feb9126dd9a
curb4387 <140212567+curb4387@users.noreply.github.com>
2024-10-09 22:24:38 +0000

    Testing Wed Oct 09 2024 22:24:38 GMT+0000 (Coordinated Universal Time)

--- chapter9/ex03/student/Computation.cpp
+++ chapter9/ex03/student/Computation.cpp
@@ -21,18 +21,31 @@ int main()
    cin >> value2; 
 		
    // Call calculateSum
-   
+   sum(value1, value2);
    // Call calculateDifference
-   
+   difference(value1, value2);
    // Call calculateProduct 
-   
+   product(value1, value2);
    return 0;
 } // End of main() function
 	
 // Write calculateSum function here
-
+void sum(double value1, double value2) {
+   double sum = value1 + value2;
+   cout << "The sum is " << sum << endl;
+   return;
+}
 
 // Write calculateDifference function here
-
+void difference(double value1, double value2) {
+   double difference =  value1 - value2;
+   cout << "The difference is " << difference << endl;
+   return;
+}
 
 // Write calculateProduct function here
+void product(double value1, double value2) {
+   double product = value1 * value2;
+   cout << "The product is " << product << endl;
+   return;
+}
