0c6289d289025c311d7d01f2ba6c6f10ca0cd5b4
curb4387 <140212567+curb4387@users.noreply.github.com>
2024-10-12 17:01:18 +0000

    Testing Sat Oct 12 2024 17:01:18 GMT+0000 (Coordinated Universal Time)

--- chapter9/ex08/student/ChangeCase.cpp
+++ chapter9/ex08/student/ChangeCase.cpp
@@ -5,6 +5,8 @@
 #include <string>
 using namespace std;
 
+string convertToString(char*, const int);
+
 int main() 
 {
    const int LEN = 9;
@@ -13,27 +15,34 @@ int main()
    char result1;
    char result2;
    int i; 
-
+   string toLower = "";
+   string toUpper = "";
    cout << "Enter 9 lowercase characters: ";
    for(i = 0; i < LEN; i++)		 
    {
       cin >> sample1[i];
-
-      // Convert sample1[i] to uppercase and assign it to result1
-      result1 = toupper(sample1[i]);
-      cout << result1 << endl;
+      sample1[i] = toupper(sample1[i]);
+      toUpper = convertToString(sample1, LEN);
    }
+   cout << toUpper << endl;
    cin.ignore(256,'\n');
    cout << "Enter 9 uppercase characters: ";
    for(i = 0; i < LEN; i++)		 
    {
       cin >> sample2[i];
-
-      // Convert sample2[i] to lowercase and assign it to result2
-      result2 = tolower(sample2[i]);
-      cout << result2 << endl;
+      sample2[i] = tolower(sample2[i]);
+      toLower = convertToString(sample2, LEN);
    }
+   cout << toLower << endl;
    cin.ignore(256,'\n');
    return 0;
 
 } // End of main() function
+string convertToString(char* sample, const int LEN) {
+   int i;
+   string converted = "";
+   for (i = 0; i < LEN; i++) {
+      converted = converted + sample[i];
+   }
+   return converted;
+}
