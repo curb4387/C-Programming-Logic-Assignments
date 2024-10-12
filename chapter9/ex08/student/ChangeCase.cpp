3e081eb7a4ab227d144358fdf5206ffd1905edea
curb4387 <140212567+curb4387@users.noreply.github.com>
2024-10-12 04:37:37 +0000

    Testing Sat Oct 12 2024 04:37:37 GMT+0000 (Coordinated Universal Time)

--- chapter9/ex08/student/ChangeCase.cpp
+++ chapter9/ex08/student/ChangeCase.cpp
@@ -15,24 +15,24 @@ int main()
    int i; 
 
    cout << "Enter 9 lowercase characters: ";
+   
    for(i = 0; i < LEN; i++)		 
    {
       cin >> sample1[i];
-
       // Convert sample1[i] to uppercase and assign it to result1
-
-      cout << result1 << endl;
+      result1 = putchar(toupper(sample1[i]));
    }
+   cout << result1 << endl;
    cin.ignore(256,'\n');
+   
    cout << "Enter 9 uppercase characters: ";
    for(i = 0; i < LEN; i++)		 
    {
       cin >> sample2[i];
-
       // Convert sample2[i] to lowercase and assign it to result2
-
-      cout << result2 << endl;
+      result2 = putchar(tolower(sample2[i]));
    }
+   cout << result2 << endl;
    cin.ignore(256,'\n');
    return 0;
 
