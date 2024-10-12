8e843a99698dd8f4431fe0e66d3dca9fbfe719c7
curb4387 <140212567+curb4387@users.noreply.github.com>
2024-10-12 00:51:02 +0000

    Testing Sat Oct 12 2024 00:51:02 GMT+0000 (Coordinated Universal Time)

--- chapter9/ex05/student/Reverse.cpp
+++ chapter9/ex05/student/Reverse.cpp
@@ -13,12 +13,27 @@ int main()
    int numbers[] = {9, 8, 7, 6, 5};
    int x;
    // Print contents of array
-                	
+   x = 5;
+   cout << "Original contents of array:" << endl;
+   for(int i = 0; i < x; i++) {
+      cout << numbers[i] << endl;
+   }
    // Call reverseArray() function here		
-   
+   reverseArray(numbers);
    // Print contents of reversed array
-     
+   cout << "Reversed contents of array:" << endl;
+   for(int i = 0; i < x; i++) {
+      cout << numbers[i] << endl;
+   }
    return 0;
 } // End of main() function
 	
 // Write reverseArray function here.
+void reverseArray(int numbers[]) {
+   for(int x = 0; x < 5 / 2; x++) {
+      int temp = numbers[x];
+      numbers[x] = numbers[5 - x - 1];
+      numbers[5 - x - 1] = temp;
+return;
+   }
+}
