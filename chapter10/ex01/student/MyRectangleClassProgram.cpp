fbc2cde06c7f3b7da040ee70e633185b2cda4212
curb4387 <140212567+curb4387@users.noreply.github.com>
2024-10-15 00:51:28 +0000

    Testing Tue Oct 15 2024 00:51:28 GMT+0000 (Coordinated Universal Time)

--- chapter10/ex01/student/MyRectangleClassProgram.cpp
+++ chapter10/ex01/student/MyRectangleClassProgram.cpp
@@ -7,10 +7,18 @@ int main()
 {
    
    // Create Rectangle objects here
-   
-   // Set the lengths and widths here
+   Rectangle rectangle1;
+   Rectangle rectangle2;
 
+   // Set the lengths and widths here
+   rectangle1.setLength(10.0);
+   rectangle1.setWidth(5.0);
+   rectangle2.setLength(7.0);
+   rectangle2.setWidth(3.0);
    // Print the areas and perimeters here
-   
+   cout << "Perimeter of rectangle1 is " << rectangle1.calculatePerimeter() << endl;
+   cout << "Area of rectangle1 is " << rectangle1.calculateArea() << endl;
+   cout << "Perimeter of rectangle2 is " << rectangle2.calculatePerimeter() << endl;
+   cout << "Area of rectangle2 is " << rectangle2.calculateArea() << endl;
    return 0;
 }
--- chapter10/ex01/student/RectangleImp.cpp
+++ chapter10/ex01/student/RectangleImp.cpp
@@ -4,37 +4,59 @@ namespace RectangleImp {
    {
       public: 
          // Declare public methods here
+         void setLength(double len);
+         double getLength();
+         void setWidth(double wid);
+         double getWidth();
+         double calculateArea();
+         double calculatePerimeter();
       private:	
          // Create length and width here
+         double length;
+         double width;
    }; 
 
    void Rectangle::setLength(double len)
    {
       // Write setLength here
+      length = len;
+      return;
    }
 
    void Rectangle::setWidth(double wid)
    {
       // write setWidth here
+      width = wid;
+      return;
    }
 
    double Rectangle::getLength()
    {
       // write getLength here
+      return length;
    }
 
    double Rectangle::getWidth()
    {
       // write getWidth here
+      return width;
    }
       
    double Rectangle::calculateArea()
    {
       // write calculateArea here
+      getLength();
+      getWidth();
+      double area = length * width;
+      return area;
    }
 
    double Rectangle::calculatePerimeter()
    {
       // write calculatePerimeter here
+      getLength();
+      getWidth();
+      double perimeter = 2 * (length + width);
+      return perimeter;
    }
 }
