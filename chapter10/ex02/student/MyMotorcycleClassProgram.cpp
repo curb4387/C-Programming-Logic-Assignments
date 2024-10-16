dda11e3dbbaf56b30c9f62fa2866f1d68925d0fe
curb4387 <140212567+curb4387@users.noreply.github.com>
2024-10-16 23:58:42 +0000

    Reset Exercise Wed Oct 16 2024 23:58:42 GMT+0000 (Coordinated Universal Time)

--- chapter10/ex02/student/MyMotorcycleClassProgram.cpp
+++ chapter10/ex02/student/MyMotorcycleClassProgram.cpp
@@ -5,36 +5,20 @@ using namespace std;
 int main()
 {
    // Create Motorcyle objects here 
-   Motorcycle motorcycleOne;
-   Motorcycle motorcycleTwo;
+
    // Create a boolean variable for side car status   
-   bool sidecarStatus;
+   
    // Set side car status here
-   motorcycleOne.setSidecar(true);
-   motorcycleTwo.setSidecar(false);
+
    // Set maximum speed here
-   motorcycleOne.setMaxSpeed(90.0);
-   motorcycleTwo.setMaxSpeed(85.0);
+
    // Set current speed here
-   motorcycleOne.setSpeed(65.0);
-   motorcycleTwo.setSpeed(60.0);
+
    // Accelerate motorcyles here
-   motorcycleOne.accelerate(30.0);
-   motorcycleTwo.accelerate(20.0);
+
    // Display current speed here
-   cout << "The current speed of motorcycleOne is " << motorcycleOne.getSpeed() << endl;
-   cout << "The current speed of motorcycleTwo is " << motorcycleTwo.getSpeed() << endl;
+   
    // Determine side car status and display results
-   sidecarStatus = motorcycleOne.getSidecar();
-   if(sidecarStatus == true) {
-        cout << "This motorcycle has a side car" << endl;
-    } else {
-        cout << "This motorcycle does not have a side car" << endl;
-    }
-   sidecarStatus = motorcycleTwo.getSidecar();
-   if(sidecarStatus == true) {
-        cout << "This motorcycle has a side car" << endl;
-    } else {
-        cout << "This motorcycle does not have a side car" << endl;
-    }
+   
+   return 0;
 }
