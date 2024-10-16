8b6b58b124baeeb65de7aad2a9efdffaa1ab74fd
curb4387 <140212567+curb4387@users.noreply.github.com>
2024-10-16 22:49:38 +0000

    Testing Wed Oct 16 2024 22:49:38 GMT+0000 (Coordinated Universal Time)

--- chapter10/ex02/student/Motorcycle.cpp
+++ chapter10/ex02/student/Motorcycle.cpp
@@ -4,3 +4,38 @@
 using namespace std; 
 
 // Write the Motorcycle class here
+class Motorcycle : public Vehicle {
+    private:
+        bool sidecar;
+        double fuelCapacity;
+        double maxSpeed;
+        double currentSpeed;
+    public:
+        void setSidecar(bool);
+        bool getSidecar();
+        void accelerate(double);
+
+};
+
+void Motorcycle::setSidecar(bool side)
+{
+    sidecar = side;
+    if(sidecar == true) {
+        cout << "This motorcycle has a side car" << endl;
+    } else {
+        cout << "This motorcycle does not have a side car" << endl;
+    }
+}
+
+bool Motorcycle::getSidecar()
+{
+    return sidecar;
+}
+
+void Motorcycle::accelerate(double mph)
+{
+   if(currentSpeed + mph < maxSpeed)
+      currentSpeed = currentSpeed + mph;
+   else
+      cout << "This motorcycle cannot go that fast." << endl; 
+}
--- chapter10/ex02/student/MyMotorcycleClassProgram.cpp
+++ chapter10/ex02/student/MyMotorcycleClassProgram.cpp
@@ -5,20 +5,26 @@ using namespace std;
 int main()
 {
    // Create Motorcyle objects here 
-
+   Motorcycle motorcycleOne;
+   Motorcycle motorcycleTwo;
    // Create a boolean variable for side car status   
-   
+   bool sidecarStatus;
    // Set side car status here
-
+   motorcycleOne.setSidecar(true);
+   motorcycleTwo.setSidecar(false);
    // Set maximum speed here
-
+   motorcycleOne.setMaxSpeed(90);
+   motorcycleTwo.setMaxSpeed(85);
    // Set current speed here
-
+   motorcycleOne.setSpeed(65);
+   motorcycleTwo.setSpeed(80);
    // Accelerate motorcyles here
-
+   motorcycleOne.accelerate(30);
+   motorcycleTwo.accelerate(20);
    // Display current speed here
-   
+   cout << "The current speed of motorcycleOne is " << motorcycleOne.getSpeed() << endl;
+   cout << "The current speed of motorcycleTwo is " << motorcycleTwo.getSpeed() << endl;
    // Determine side car status and display results
-   
-   return 0;
+   motorcycleOne.getSidecar();
+   motorcycleTwo.getSidecar();
 }
