7809c8f08bd8f87ed672634effca9f80f7fdd1bb
curb4387 <140212567+curb4387@users.noreply.github.com>
2024-10-17 00:05:51 +0000

    Testing Thu Oct 17 2024 00:05:51 GMT+0000 (Coordinated Universal Time)

--- chapter10/ex02/student/Motorcycle.cpp
+++ chapter10/ex02/student/Motorcycle.cpp
@@ -13,3 +13,21 @@ class Motorcycle: public Vehicle
     private:
         bool sidecar;
 };
+
+void Motorcycle::accelerate(double mph)
+{
+   if(getSpeed() + mph > getMaxSpeed())
+      cout << "This motorcycle cannot go that fast" << endl;
+   else
+      setSpeed(getSpeed() + mph);
+}
+
+void Motorcycle::setSidecar(bool status)
+{
+    sidecar = status;
+}
+
+bool Motorcycle::getSidecar()
+{
+    return sidecar;
+}
