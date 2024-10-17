20698f08eab969f811b29e41c2084e150f33ad08
curb4387 <140212567+curb4387@users.noreply.github.com>
2024-10-17 00:20:46 +0000

    Testing Thu Oct 17 2024 00:20:45 GMT+0000 (Coordinated Universal Time)

--- chapter10/ex02/student/Motorcycle.cpp
+++ chapter10/ex02/student/Motorcycle.cpp
@@ -2,30 +2,32 @@
 #include "Vehicle.cpp"
 #include <iostream>
 using namespace std; 
-class Motorcycle : public Vehicle
+
+// Write the Motorcycle class here
+class Motorcycle: public Vehicle
 {
-   public: 
-      void accelerate(double); 
-      void setSideCar(bool);
-      bool getSideCar();
-   private: 
-      bool sidecar; 
-}; 
+    public:
+        void accelerate(double);
+        void setSidecar(bool);
+        bool getSidecar();
+    private:
+        bool sidecar;
+};
 
 void Motorcycle::accelerate(double mph)
 {
    if(getSpeed() + mph > getMaxSpeed())
-      cout << "This motorcycle cannot go that fast" << endl; 
+      cout << "This motorcycle cannot go that fast" << endl;
    else
       setSpeed(getSpeed() + mph);
 }
 
-void Motorcycle::setSideCar(bool status)
+void Motorcycle::setSidecar(bool status)
 {
-   sidecar = status;
+    sidecar = status;
 }
 
-bool Motorcycle::getSideCar()
+bool Motorcycle::getSidecar()
 {
-   return sidecar;
+    return sidecar;
 }
